e1446791fa757a88dd0b94629c6a21b7ab061ae6
Nick Wong <njwon4@gmail.com>
2023-09-04 21:00:20 +1000

    feat(raven-api): add arms endpoint

--- apps/raven-api/src/v1/parts/_router.ts
+++ apps/raven-api/src/v1/parts/_router.ts
@@ -2,11 +2,12 @@ import { Router } from 'itty-router'
 import regulationVersionRouter from './regulation_version'
 import headRouter from './head'
 import coreRouter from './core'
+import armsRouter from './arms'
 
 const router = Router({ base: '/api/v1/parts' })
 
 router.get('/regulation_version/*', regulationVersionRouter.handle)
 router.get('/head/*', headRouter.handle)
-router.get('/core/*', coreRouter.handle)
+router.get('/arms/*', armsRouter.handle)
 
 export default router
