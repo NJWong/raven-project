a9dc8e4e1e8c48b74b993bc52580d49c451f928e
Nick Wong <njwon4@gmail.com>
2023-09-04 20:40:00 +1000

    feat(raven-api): add core endpoint

--- apps/raven-api/src/v1/parts/_router.ts
+++ apps/raven-api/src/v1/parts/_router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'itty-router'
 import regulationVersionRouter from './regulation_version'
 import headRouter from './head'
+import coreRouter from './core'
 
 const router = Router({ base: '/api/v1/parts' })
 
 router.get('/regulation_version/*', regulationVersionRouter.handle)
 router.get('/head/*', headRouter.handle)
+router.get('/core/*', coreRouter.handle)
 
 export default router
