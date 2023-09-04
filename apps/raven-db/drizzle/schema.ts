1fe09a292c8924d1058d4ecaeda02dcb42e1e6ce
Nick Wong <njwon4@gmail.com>
2023-09-04 20:55:06 +1000

    refactor(raven-db): change attitudeStability from real to integer

--- apps/raven-db/drizzle/schema.ts
+++ apps/raven-db/drizzle/schema.ts
@@ -13,22 +13,6 @@ export const regulationVersion = sqliteTable("regulation_version", {
 	date: text("date").notNull(),
 });
 
-export const core = sqliteTable("core", {
-	id: integer("id").primaryKey().notNull(),
-	regulationVersion: text("regulation_version").notNull(),
-	name: text("name").notNull(),
-	ap: integer("ap").notNull(),
-	antiKineticDefense: integer("anti_kinetic_defense").notNull(),
-	antiEnergyDefense: integer("anti_energy_defense").notNull(),
-	antiExplosiveDefense: integer("anti_explosive_defense").notNull(),
-	attitudeStability: real("attitude_stability").notNull(),
-	boosterEfficiencyAdj: integer("booster_efficiency_adj").notNull(),
-	generatorOutputAdj: integer("generator_output_adj").notNull(),
-	generatorSupplyAdj: integer("generator_supply_adj").notNull(),
-	weight: integer("weight").notNull(),
-	enLoad: integer("en_load").notNull(),
-});
-
 export const head = sqliteTable("head", {
 	id: integer("id").primaryKey().notNull(),
 	regulationVersion: text("regulation_version").notNull(),
@@ -41,6 +25,22 @@ export const head = sqliteTable("head", {
 	systemRecovery: integer("system_recovery").notNull(),
 	scanDistance: integer("scan_distance").notNull(),
 	scanEffectDuration: real("scan_effect_duration").notNull(),
+	weight: integer("weight").notNull(),
+	enLoad: integer("en_load").notNull(),
+});
+
+export const core = sqliteTable("core", {
+	id: integer("id").primaryKey().notNull(),
+	regulationVersion: text("regulation_version").notNull(),
+	name: text("name").notNull(),
+	ap: integer("ap").notNull(),
+	antiKineticDefense: integer("anti_kinetic_defense").notNull(),
+	antiEnergyDefense: integer("anti_energy_defense").notNull(),
+	antiExplosiveDefense: integer("anti_explosive_defense").notNull(),
+	attitudeStability: integer("attitude_stability").notNull(),
+	boosterEfficiencyAdj: integer("booster_efficiency_adj").notNull(),
+	generatorOutputAdj: integer("generator_output_adj").notNull(),
+	generatorSupplyAdj: integer("generator_supply_adj").notNull(),
 	weight: integer("weight").notNull(),
 	enLoad: integer("en_load").notNull(),
 });
